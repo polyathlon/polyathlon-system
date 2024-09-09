4e40c4a5d2a1f0a8537b7aef15a9da05323e7736
v-a-antoshkin <v.a.antoshkin@mail.ru>
2024-09-09 14:58:03 +0300

    Country DataSet Add

---
+++ js/polyathlon-system/refresh-token.mjs
@@ -0,0 +1,32 @@
+export default async function refreshToken() {
+    const response = await fetch('https://localhost:4500/api/refresh-token', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        credentials: "include",
+    })
+
+    const result = await response.json()
+    if (!response.ok) {
+        throw new Error(result.error)
+    }
+
+    const token = result.token
+    saveToken(token)
+    return token
+}
+
+export function  getToken() {
+    return localStorage.getItem('rememberMe') ? localStorage.getItem('accessUserToken') : sessionStorage.getItem('accessUserToken')
+}
+
+function saveToken(token) {
+    if (localStorage.getItem('rememberMe')) {
+        localStorage.setItem('accessUserToken', token)
+    }
+    else {
+        sessionStorage.setItem('accessUserToken', token)
+    }
+    return token;
+}
