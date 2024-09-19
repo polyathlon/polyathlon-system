b742e30056bb2589776c324ea376c860992bda9a
v-a-antoshkin <v.a.antoshkin@mail.ru>
2024-09-19 18:58:29 +0300

    Fix Age Group Bug

--- js/polyathlon-system/pages/my-age-groups/my-age-groups-dataset.mjs
+++ js/polyathlon-system/pages/my-age-groups/my-age-groups-dataset.mjs
@@ -1,41 +1,167 @@
-import { BaseElement, html, css} from '../../../base-element.mjs'
+import refreshToken, {getToken} from "../../refresh-token.mjs";
 
-import './my-age-groups-section-1.mjs';
-import '../../../../components/buttons/aside-button.mjs';
+export default class DataSet {
+    static #dataSet;
 
-class MyCompetitionTypes extends BaseElement {
-    static get styles() {
-        return [
-            css`
-                :host {
-                    display: flex;
-                    box-sizing: border-box;
-                    height: 100%
-                }
-                aside {
-                    display: flex;
-                    flex-direction: column;
-                }
-            `
-        ]
-    }
-
-    static get properties() {
-        return {
-            version: { type: String, default: '1.0.0', save: true },
+    static async getDataSet() {
+        if (DataSet.#dataSet) {
+            return DataSet.#dataSet
         }
+        DataSet.#dataSet = await DataSet.#getItems()
+        return DataSet.#dataSet
     }
 
-    constructor() {
-        super();
-        this.version = "1.0.0";
+    static #fetchGetItems(token) {
+        return fetch('https://localhost:4500/api/age-groups', {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
     }
 
-    render() {
-        return html`
-            <my-age-groups-section-1></my-age-groups-section-1>
-        `;
+    static async #getItems() {
+        const token = getToken()
+        let response = await DataSet.#fetchGetItems(token)
+        if (response.status === 419) {
+            const token = await refreshToken()
+            response = await DataSet.#fetchGetItems(token)
+        }
+        const result = await response.json()
+        if (!response.ok) {
+            throw new Error(result.error)
+        }
+        const items = result.rows.map(item => {
+            return item.doc;
+        })
+        return items
+    }
+
+    static fetchAddItem(token) {
+        const newItem = {name: "Новый город"}
+        return fetch(`https://localhost:4500/api/age-group`, {
+            method: "POST",
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(newItem)
+        })
+    }
+
+    static async addItem() {
+        const token = getToken();
+        let response = await DataSet.fetchAddItem(token)
+
+        if (response.status === 419) {
+            const token = await refreshToken()
+            response = await DataSet.fetchAddItem(token)
+        }
+        const result = await response.json()
+        if (!response.ok) {
+            throw new Error(result.error)
+        }
+
+        const item = await DataSet.getItem(result.id)
+        DataSet.addToDataset(item)
+        return item
+    }
+
+    static addToDataset(item) {
+        DataSet.#dataSet.unshift(item);
+    }
+
+    static #fetchGetItem(token, itemId) {
+        return fetch(`https://localhost:4500/api/age-group/${itemId}`, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
+    }
+
+    static async getItem(itemId) {
+        const token = getToken();
+
+        let response = await DataSet.#fetchGetItem(token, itemId)
+
+        if (response.status === 419) {
+            const token = await refreshToken()
+            response = await DataSet.#fetchGetItem(token, itemId)
+        }
+
+        const result = await response.json()
+
+        if (!response.ok) {
+            throw new Error(result.error)
+        }
+        return result
+    }
+
+    static #fetchSaveItem(token, item) {
+        return fetch(`https://localhost:4500/api/age-group/${item._id}`, {
+            method: "PUT",
+            headers: {
+              'Authorization': `Bearer ${token}`,
+              'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(item)
+        })
+    }
+
+    static async saveItem(item) {
+        const token = getToken();
+
+        let response = await DataSet.#fetchSaveItem(token, item)
+
+        if (response.status === 419) {
+            const token = await refreshToken()
+            response = await DataSet.#fetchSaveItem(token, item)
+        }
+
+        const result = await response.json()
+
+        if (!response.ok) {
+            throw new Error(result.error)
+        }
+        DataSet.#afterSave(item, result)
+    }
+
+    static #afterSave(item, itemHeader) {
+        item._rev = itemHeader.rev;
+    }
+
+    static #fetchDeleteItem(token, item) {
+        return fetch(`https://localhost:4500/api/age-group/${item._id}?rev=${item._rev}`, {
+            method: "DELETE",
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
+    }
+
+    static async deleteItem(item) {
+        const token = getToken();
+
+        let response = await DataSet.#fetchDeleteItem(token, item)
+
+        if (response.status === 419) {
+            const token = await refreshToken()
+            response = await DataSet.#fetchDeleteItem(token, item)
+        }
+
+        const result = await response.json()
+
+        if (!response.ok) {
+            throw new Error(result.error)
+        }
+
+        DataSet.#deleteFromDS(item)
+    }
+
+    static #deleteFromDS(item) {
+        const itemIndex = DataSet.#dataSet.indexOf(item)
+        if (itemIndex === -1) {
+            return
+        }
+        DataSet.#dataSet.splice(itemIndex, 1)
     }
 }
-
-customElements.define("my-age-groups", MyCompetitionTypes);
--- js/polyathlon-system/pages/my-age-groups/my-age-groups-section-1-page-1.mjs
+++ js/polyathlon-system/pages/my-age-groups/my-age-groups-section-1-page-1.mjs
@@ -31,6 +31,8 @@ class MyAgeGroupsSection1Page1 extends BaseElement {
         return html`
             <div>
                 <simple-input id="name" icon-name="user" label="Gender Age name:" .value=${this.item?.name} @input=${this.validateInput}></simple-input>
+                <simple-input id="minAge" icon-name="user" label="Min Age:" .value=${this.item?.minAge} @input=${this.validateInput}></simple-input>
+                <simple-input id="maxAge" icon-name="user" label="Max Age:" .value=${this.item?.maxAge} @input=${this.validateInput}></simple-input>
             </div>
         `;
     }
